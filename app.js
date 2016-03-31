8434f633aaeba8a21cfa75c240a3cb9744b76363
Gum-Joe <kishansambhi@hotmail.co.uk>
2016-03-31 15:06:51 +0100

    Add main files

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Entry script for buildup
+ */
+
+/**
+ * Module dependencies
+ */
+const server = require('./lib/server.js');
+
+module.exports = { Server: server.Server }
---
+++ bin/cli.js
@@ -0,0 +1,17 @@
+
+/*
+ * CLI for buildup
+ */
+
+
+/*
+ * Module dependencies
+ */
+
+const com = require('commander');
+
+/**
+ * Setup
+ */
+com
+  .version(require('../package').version)
