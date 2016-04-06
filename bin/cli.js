08d869e7280d239e3abf65c3e61a0e137a1073d3
Gum-Joe <kishansambhi@hotmail.co.uk>
2016-04-06 12:12:21 +0100

    Start adding build
    :art: :construction: Added downloading

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,4 @@
-
+'use strict'
 /*
  * CLI for buildup
  */
@@ -7,11 +7,26 @@
 /*
  * Module dependencies
  */
-
+const app = require('../app');
 const com = require('commander');
-
+let taskrun;
 /**
  * Setup
  */
 com
   .version(require('../package').version)
+  .usage('<task> [options...]')
+  .action(function (task, options) {
+     taskrun = task;
+  })
+  .option('-v, --verbose', 'Verbose logging')
+  .parse(process.argv);
+
+if (typeof taskrun === 'undefined') {
+  taskrun = 'default'
+}
+var ser = new app.Server('./servertest', { quite: false })
+ser.nameServer('buildtest')
+ser.loadConfig(`buildup.json`, { file: true, type: 'json' })
+ser.loadPlugins()
+ser.runTask(taskrun)
